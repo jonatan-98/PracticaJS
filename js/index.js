3ae714e00d19e424870b920af46a02ecc48ac645
Jona Rodriguez <jonrodriguez@alumno.unlam.edu.ar>
2022-08-16 02:25:22 -0300

    agregado de HTML y CSS

--- js/index.js
+++ js/index.js
@@ -46,7 +46,21 @@ function validacion(usuario, clave) {
             if(usuarios.length == i+1){
                 alert(`Usuario incorrecto.`)
             }
-            }
+        }
         
     }
 }
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+// Evento para abrir form de registro
+signUpButton.addEventListener('click', () =>
+  container.classList.add('right-panel-active')
+);
+
+// Evento para regresar al form de iniciar sesión
+signInButton.addEventListener('click', () =>
+  container.classList.remove('right-panel-active')
+);
